c0edb011a2791d7d7349888544b8682b5a1bb128
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2023-02-18 13:46:18 -0300

    Subida de archivos

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import ProductManager from './ProductManager.js';
+import CartManager from './CartManager.js'
+import productsRoutes from './routes/products.router.js'
+import cartsRoutes from './routes/carts.router.js'
+
+export const productManager = new ProductManager();
+export const cartManager = new CartManager(); 
+
+const app = express();
+const BASE_PREFIX = "/api"
+
+app.listen(8080);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(`${BASE_PREFIX}/products`, productsRoutes);
+app.use(`${BASE_PREFIX}/carts`, cartsRoutes);
