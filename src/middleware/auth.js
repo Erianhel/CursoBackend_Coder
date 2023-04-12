747de904b6cc6ee923296ee9ffb6ef2d36f5ed2e
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2023-04-12 20:05:10 -0300

    Delete src directory

--- src/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-const authMdw = (req, res, next)=>{
-    console.log("Revisando la session: ", req.session);
-    if (req.session?.user) {
-        return next();
-    }
-    return res.redirect("/login");
-}
-
-export default authMdw;
