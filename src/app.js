b8c98cdde4f3b7009061684d0cf221abc2cf79e9
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2023-05-06 23:38:44 -0300

    Aniado politicas y capas

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(session({
     store: MongoStore.create({
-        mongoUrl:"mongodb+srv://leaapagro:1234@ecommerce.h9vznv2.mongodb.net/?retryWrites=true&w=majority",
+        mongoUrl:env.mongoUrl,
         mongoOptions:{useNewUrlParser: true, useUnifiedTopology:true},
         ttl:60*3600
     }),
