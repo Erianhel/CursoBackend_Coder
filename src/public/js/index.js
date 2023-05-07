bfefa0d9660765703c473c854237dbd6d769de4e
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2023-05-07 03:51:10 -0300

    cambios menores

--- src/public/js/index.js
+++
@@ -1,5 +0,0 @@
-const socket = io();
-
-socket.on('addProduct', ()=> document.location.reload());
-socket.on('deleteProduct', ()=> document.location.reload());
-socket.on('updatedProduct', ()=> document.location.reload());
