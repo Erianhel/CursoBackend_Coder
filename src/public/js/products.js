71242d47e5aa38916dfbdf2fac2e05e28189fec6
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2023-05-13 17:59:16 -0300

    Realizo cambios

--- src/public/js/products.js
+++
@@ -1,8 +0,0 @@
-const agregarAlCarrito = (_id,cart)=>{
-    console.log("Este es el id del producto", _id);
-    console.log("Tipo de dato del carrito", cart);
-
-    fetch(`http://localhost:8080/api/carts/${cart}/products/${_id}`, {
-        method: 'POST'
-     })
-}
