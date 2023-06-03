ad02e29fcab93200c8216cf563017734b335cfbf
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2023-06-03 20:58:02 -0300

    Realizo cambios

--- src/routes/loggerTest.router.js
+++
@@ -1,8 +0,0 @@
-import { getTest } from "../controllers/logger.controller.js";
-import CustomRouter from "./router.router.js";
-
-export default class LoggerRouter extends CustomRouter {
-  init() {
-    this.get("/", ["PUBLIC"], getTest);
-  }
-}
